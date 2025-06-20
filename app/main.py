4c60cd8233784121e285b6aeda04906177ef1e09
NyuydineBill <billleynyuy@gmail.com>
2025-06-20 14:42:44 +0100

    Added origins

--- app/main.py
+++ app/main.py
@@ -39,7 +39,9 @@ app.add_middleware(TrailingSlashMiddleware)
 origins = [
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:8000",
     "http://localhost:3000",
+    "https://server.sui-ru.com",
     # Add other origins as needed
 ]
 
